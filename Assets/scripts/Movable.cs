660c422242fcd0637873e0eb75be3e0dc93b6dc1
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-23 08:23:35 +0200

    Created basic platformer behaviour

---
+++ Assets/scripts/Movable.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movable : MonoBehaviour {
+
+	public float jumpVelocity = 1f;
+	public float horizontalVelocity = 1f;
+    public int maxJumpFrames = 10;
+
+	public float horizontalAxisDeadzone = 0.1f;
+	public float verticalAxisDeadzone = 0.1f;
+
+    public GameObject[] jumpLocks;
+
+    int jumpTimeout;
+
+	void FixedUpdate () {
+        var velocity = rigidbody2D.velocity;
+
+		var horizontal = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs (horizontal) > horizontalAxisDeadzone)
+        {
+            velocity.x = horizontalVelocity * horizontal;
+        }
+        else
+        {
+            velocity.x = 0;
+        }
+
+        if (IsOnGround ())
+        {
+            jumpTimeout = maxJumpFrames;
+        }
+
+        var vertical = Input.GetAxis ("Vertical");
+        if (vertical > verticalAxisDeadzone && jumpTimeout > 0f)
+        {
+            velocity.y = jumpVelocity;
+            --jumpTimeout;
+        }
+        else
+        {
+            jumpTimeout = 0;
+        }
+
+        rigidbody2D.velocity = velocity;
+	}
+
+    bool IsOnGround()
+    {
+        var pos2D = Vector2.zero;
+        foreach (var jumpLock in jumpLocks)
+        {
+            var pos = jumpLock.transform.position;
+            pos2D.x = pos.x;
+            pos2D.y = pos.y;
+            var result = Physics2D.Raycast(pos2D, Vector2.zero);
+            if (result.collider != null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/scripts/SnapToGrid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class SnapToGrid : MonoBehaviour {
+	const int DEFAULT_GRID = 1;
+
+	public int gridX = DEFAULT_GRID;
+	public int gridY = DEFAULT_GRID;
+
+	void Start () {
+		if (!Application.isEditor)
+		{
+			enabled = false;
+		} 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (gridX <= 0) { gridX = DEFAULT_GRID; }
+		if (gridY <= 0) { gridY = DEFAULT_GRID; }
+
+        var oldPosition = transform.position;
+		var position = transform.position;
+		position.x = Mathf.RoundToInt(position.x / gridX) * gridX;
+		position.y = Mathf.RoundToInt(position.y / gridY) * gridY;
+        var distance = Vector3.Distance (oldPosition, position);
+        if (!Mathf.Approximately (distance, 0f))
+        {
+            transform.position = position;
+        }
+	}
+}
