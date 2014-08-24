3ffd29474f90ccd534e86371fa1e24b1d8fa7065
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 10:13:13 +0200

    Added bomb prefab

---
+++ Assets/scripts/RotateTween.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTween : MonoBehaviour {
+
+    public Vector3 rotation;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotation * Time.deltaTime);
+	}
+}
