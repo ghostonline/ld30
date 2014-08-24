84238996b6b644ed2b64daf724447c9f709a619a
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 11:31:22 +0200

    Created a health bar

---
+++ Assets/scripts/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class HealthBar : MonoBehaviour {
+
+    public Transform background;
+    public Transform bar;
+    public SpriteRenderer barSprite;
+
+    public Color fullColor;
+    public Color emptyColor;
+
+    public float value;
+
+    static float colorValueEase(float input)
+    {
+        return input*(2-input);
+    }
+
+	void Update () {
+        if (bar == null || background == null) { return; }
+
+        var barScale = bar.localScale;
+        var normalizedValue = Mathf.Clamp01(value);
+        var targetScale = background.localScale.x * normalizedValue;
+        if (!Mathf.Approximately(barScale.x, targetScale))
+        {
+            barScale.x = targetScale;
+            bar.localScale = barScale;
+
+            if (barSprite != null)
+            {
+                var colorValue = colorValueEase(normalizedValue);
+                var color = Color.Lerp(emptyColor, fullColor, colorValue);
+                barSprite.color = color;
+            }
+        }
+	}
+}
