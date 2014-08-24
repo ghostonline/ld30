704dec5b37f51658f959b6368d6779eaa1d26899
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 11:31:57 +0200

    Bombs have a health bar

---
+++ Assets/scripts/Damagable.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damagable : MonoBehaviour {
+
+    public Transform healthBarAnchor;
+    public HealthBar barTemplate;
+    public float barDecreaseSpeed;
+    public int maxHealth;
+    public int currentHealth;
+
+    HealthBar healthBar;
+    bool updateBar;
+
+	// Use this for initialization
+	void Start () {
+        if (barTemplate == null || healthBarAnchor == null)
+        {
+            Debug.LogWarning("Cannot create a health bar for damagable gameobject");
+            return;
+        }
+
+        healthBar = (HealthBar)GameObject.Instantiate(barTemplate);
+        healthBar.transform.parent = healthBarAnchor;
+        healthBar.transform.localPosition = Vector3.zero;
+        updateBar = true;
+
+        if (barDecreaseSpeed <= 0) { barDecreaseSpeed = 0.1f; }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!updateBar) { return; }
+
+        var desiredValue = Mathf.Clamp01((float)currentHealth / (float)maxHealth);
+        var diff = healthBar.value - desiredValue;
+        if (!Mathf.Approximately(diff, 0f))
+        {
+            if (diff > 0)
+            {
+                healthBar.value = Mathf.Min(barDecreaseSpeed * Time.deltaTime, diff);
+            }
+            else
+            {
+                healthBar.value = desiredValue;
+            }
+        }
+        else
+        {
+            updateBar = false;
+        }
+	}
+
+    void ApplyDamage(int points)
+    {
+        currentHealth -= points;
+        updateBar = true;
+    }
+}
