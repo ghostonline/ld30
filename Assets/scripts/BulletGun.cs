ac9d7cb333b6829596d476bea37e9c6491be3a31
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 08:22:48 +0200

    Populate a bullet pool

---
+++ Assets/scripts/BulletGun.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletGun : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public Transform fireDirection;
+
+    public GameObject bulletTemplate;
+    public Transform poolParent;
+    public int poolSize = 10;
+
+	// Use this for initialization
+	void Start () {
+        if (poolSize <= 0) { poolSize = 10; }
+        var poolParentTransform = poolParent.transform;
+        var digits = Mathf.CeilToInt(Mathf.Log10(poolSize));
+        bulletTemplate.SetActive(false);
+        string bulletTemplateName = string.Format("{0}{{0:D0{1}}}", bulletTemplate.name, digits);
+        for (int ii = 0; ii < poolSize; ++ii)
+        {
+            var child = (GameObject)GameObject.Instantiate(bulletTemplate);
+            child.name = string.Format(bulletTemplateName, ii);
+            child.transform.parent = poolParentTransform;
+            //child.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
