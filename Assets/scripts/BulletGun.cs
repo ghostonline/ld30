783db3b5a90baf04d67f88691ea815dd0a3c9f27
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 09:12:45 +0200

    Spawn bullets when pressing the fire button

--- Assets/scripts/BulletGun.cs
+++ Assets/scripts/BulletGun.cs
@@ -5,14 +5,19 @@ public class BulletGun : MonoBehaviour {
 
     public Transform spawnPoint;
     public Transform fireDirection;
+    public float fireRate;
 
     public GameObject bulletTemplate;
     public Transform poolParent;
     public int poolSize = 10;
 
-	// Use this for initialization
+    float fireCooldown;
+    GameObject[] bullets;
+    int poolIdx;
+
 	void Start () {
         if (poolSize <= 0) { poolSize = 10; }
+        bullets = new GameObject[poolSize];
         var poolParentTransform = poolParent.transform;
         var digits = Mathf.CeilToInt(Mathf.Log10(poolSize));
         bulletTemplate.SetActive(false);
@@ -22,12 +27,45 @@ public class BulletGun : MonoBehaviour {
             var child = (GameObject)GameObject.Instantiate(bulletTemplate);
             child.name = string.Format(bulletTemplateName, ii);
             child.transform.parent = poolParentTransform;
-            //child.SetActive(false);
+            bullets[ii] = child;
         }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        fireCooldown -= Time.deltaTime;
+	    if (Input.GetAxis("Fire1") > 0 && fireCooldown < 0)
+        {
+            var bullet = FindFreeBullet();
+            bullet.transform.position = spawnPoint.position;
+            bullet.transform.rotation = CalculateAimAngle();
+            bullet.SetActive(true);
+            fireCooldown = fireRate;
+        }
 	}
+
+    GameObject FindFreeBullet()
+    {
+        // Start running from the next in line
+        poolIdx = (poolIdx + 1) % poolSize;
+        for (int ii = 0; ii < poolSize; ++ii)
+        {
+            int idx = (poolIdx + ii) % poolSize;
+            if (!bullets[idx].activeSelf)
+            {
+                poolIdx = idx;
+                return bullets[idx];
+            }
+        }
+
+        // Just return the oldest one
+        return bullets[poolIdx];
+    }
+
+    Quaternion CalculateAimAngle()
+    {
+        var lookDir = Quaternion.LookRotation(fireDirection.position - spawnPoint.position);
+        var realign = Quaternion.Euler(0, 270, 0);
+        return lookDir * realign;
+    }
 }
