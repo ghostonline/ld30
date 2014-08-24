4319f4a7562c3ecc09d45cd8d0063291d7d526d1
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 07:55:16 +0200

    Allow aiming using the mouse

---
+++ Assets/scripts/Aimable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aimable : MonoBehaviour {
+
+	void Update () {
+        var worldAim = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var myPos = transform.position;
+        worldAim.z = myPos.z;
+        transform.rotation = Quaternion.LookRotation(worldAim - myPos);
+        transform.Rotate(0,270,0);
+	}
+}
