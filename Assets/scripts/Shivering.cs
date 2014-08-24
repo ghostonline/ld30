38d228e41bf9c42ae2bc872df2f12ec4b36da564
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-24 14:25:31 +0200

    Bomb has a shiver component

---
+++ Assets/scripts/Shivering.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shivering : MonoBehaviour {
+
+    public float magnitude = 1f;
+    public float speed = 1f;
+
+    Vector3 basePosition;
+    Vector3 targetPosition;
+
+	void Start () {
+        basePosition = transform.localPosition;
+        targetPosition = selectRandomDeviationTarget();
+	}
+
+    void OnEnable()
+    {
+        transform.localPosition = selectRandomDeviationTarget();
+    }
+
+    void OnDisable()
+    {
+        transform.localPosition = basePosition;
+    }
+
+    Vector3 selectRandomDeviationTarget()
+    {
+        var direction = new Vector3(Random.value - 0.5f,Random.value - 0.5f,Random.value - 0.5f);
+        direction.Normalize();
+        return basePosition + direction * magnitude;
+    }
+	
+	void Update () {
+        var curPosition = transform.localPosition;
+        var distance = Vector3.Distance(targetPosition, curPosition);
+        var travelDistance = Mathf.Min(speed * Time.deltaTime, magnitude * 2);
+        if (distance < travelDistance)
+        {
+            curPosition = targetPosition;
+            travelDistance -= distance;
+            targetPosition = selectRandomDeviationTarget();
+        }
+
+        var direction = targetPosition - curPosition;
+        direction.Normalize();
+        transform.localPosition = curPosition + direction * travelDistance;
+    }
+}
