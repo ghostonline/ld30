0e5b3c53439be02176caa10d3514d8835d2f7376
Bart Veldstra <bart.veldstra@gmail.com>
2014-08-25 03:38:14 +0200

    Enemies can (finally) damage the player

---
+++ Assets/scripts/DamagePlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamagePlayer : MonoBehaviour {
+
+    const string PlayerTag = "Player";
+    public int damage = 1;
+
+    void DoDamageIfPlayer(GameObject obj)
+    {
+        if (obj.tag == PlayerTag)
+        {
+            var damagable = obj.GetComponent<Damagable>();
+            if (damagable)
+            {
+                damagable.ApplyDamage(damage);
+            }
+        }
+    }
+
+	// Use this for initialization
+    void OnCollisionEnter2D (Collision2D coll)
+    {
+        DoDamageIfPlayer(coll.gameObject);
+	}
+
+    void OnTriggerEnter2D (Collider2D coll)
+    {
+        DoDamageIfPlayer(coll.gameObject);
+    }
+}
